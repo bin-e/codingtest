86b3917525e401c384fba36358d646c7b8f905d4
Jeong-Uk Park <dnr1105@gmail.com>
2021-10-19 21:52:22 +0900

    Clean-up code

--- Week1/quest/MyQueue.java
+++
@@ -1,9 +0,0 @@
-public class MyQueue<T> {
-
-    /**
-     * Main
-     */
-    public static void main(String[] args) {
-
-    }
-}
--- Week1/quest/MyStack.java
+++
@@ -1,9 +0,0 @@
-public class MyStack<T> {
-
-    /**
-     * Main
-     */
-    public static void main(String[] args) {
-
-    }
-}
